fe779ef830b96d52960c1b5fa1c9dc98ee4026ff
俊杰 <1131473524@qq.com>
2019-05-06 07:00:09 +0800

    添加服务端models模块

---
+++ server/models/goods.js
@@ -0,0 +1,10 @@
+//module.exports
+//mongoose 提供require类库，所以不用指定路径
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var produtSchema = new Schema({
+	//""
+})
+
+//emd amd（es5规范） exprots(node规范)
