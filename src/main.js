fd22a1422ee1104f7918852461880a9cb9b9f207
junjiezeng <zengjunj21@sina.com>
2019-05-18 13:14:47 +0800

    定义全局过滤器（金额）

--- src/main.js
+++ src/main.js
@@ -5,11 +5,17 @@ import App from './App'
 import router from './router'
 import lazyLoad from 'vue-lazyload'
 import infinite_scorll from 'vue-infinite-scroll'
+import {currency} from './util/currency.js'
+
 //css
 import './assets/css/base.css'
 import './assets/css/checkout.css'
 import './assets/css/login.css'
 import './assets/css/product.css'
+
+
+//分页插件
+Vue.use(infinite_scorll)
 
 //图片懒加载
 Vue.use(lazyLoad,{
@@ -17,8 +23,10 @@ Vue.use(lazyLoad,{
 	try:3 //默认一
 })
 
-//分页插件
-Vue.use(infinite_scorll)
+//金额全局过滤
+Vue.filter("currency",currency)
+
+
 
 Vue.config.productionTip = false
 
